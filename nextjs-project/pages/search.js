55e79ae56cf0ce6e042e799e7549010071670cbc
taopoppy <15009571633@163.com>
2020-08-21 16:38:50 +0800

    finish search

--- nextjs-project/pages/search.js
+++ nextjs-project/pages/search.js
@@ -1,6 +1,9 @@
-import Router,{ withRouter } from "next/router";
+import { withRouter } from "next/router";
 import api from '../lib/api'
-import { Row, Col,List } from 'antd'
+import { Row, Col,List, Pagination } from 'antd'
+import { memo,isValidElement } from 'react'
+import Link from 'next/link'
+import Repo from '../components/Repo'
 
 
 const LANGUAGE = ['Javascript','HTML','CSS','Typescript','Java','Rust',]
@@ -20,6 +23,24 @@ const selectedItemStyle = {
 	fontWeight: 100
 }
 
+function noop() {}
+
+const per_page = 20
+
+const FilterLink = memo(({name, query, lang, sort, order,page}) => {
+	let queryString = `?query=${query}`
+	if (lang) queryString += `&lang=${lang}`
+	if (sort) queryString += `&sort=${sort}&order=${order||'desc'}`
+	if (page) queryString += `&page=${page}`
+	queryString += `&per_page=${per_page}`
+
+	return (
+		<Link href={`/search${queryString}`}>
+			{isValidElement(name) ? name: <a>{name}</a>}
+		</Link>
+	)
+})
+
 
 /**
  * sort:排序方式
@@ -28,32 +49,8 @@ const selectedItemStyle = {
  * page：分页页面
  */
 function Search({router,repos}) {
-	const { sort, order, lang, query} = router.query
-
-	const handlelanguageChange = (language) => {
-		Router.push({
-			pathname: '/search',
-			query: {
-				query,
-				lang:language,
-				sort,
-				order
-			}
-		})
-	}
-
-	const handleSortChange = (sort) => {
-		Router.push({
-			pathname: '/search',
-			query: {
-				query,
-				lang,
-				sort:sort.value,
-				order:sort.order
-			}
-		})
-	}
-
+	const { ...querys } = router.query
+	const { lang, sort, order,page } = router.query
 
 	return (
 		<div className="root">
@@ -68,7 +65,13 @@ function Search({router,repos}) {
 							const selected = lang === item
 							return (
 								<List.Item key={item} style={selected? selectedItemStyle: null}>
-									<a onClick={()=> handlelanguageChange(item)}>{item}</a>
+									{ selected ? <span>{item}</span> :
+										<FilterLink
+											{...querys}
+											lang={item}
+											name={item}
+										/>
+									}
 								</List.Item>
 							)
 						}}
@@ -86,13 +89,57 @@ function Search({router,repos}) {
 							}
 							return (
 								<List.Item key={item.name} style={selected? selectedItemStyle: null}>
-									<a onClick={()=> handleSortChange(item)}>{item&&item.name}</a>
+									{ selected ? <span>{item.name}</span> :
+										<FilterLink
+											{...querys}
+											sort={item.value}
+											order={item.order}
+											name={item.name}
+										/>
+									}
 								</List.Item>
 							)
 						}}
 					/>
 				</Col>
+				<Col span={18}>
+					<h3 className="repos-title">{repos.total_count} 条结果</h3>
+					{
+						repos.items.map(repo => <Repo repo={repo} key={repo.id} />)
+					}
+					<div className="pagination">
+						<Pagination
+							pageSize={per_page}
+							current={Number(page) || 1}
+							total={1000}
+							onChange={noop}
+							itemRender={(page, type ,ol) => {
+								const p = type === 'page'? page: type ==='prev' ? page - 1 :page + 1
+								const name = type === 'page'? page: ol
+								return <FilterLink {...querys} page= {p} name={name}/>
+							}}
+						/>
+					</div>
+				</Col>
 			</Row>
+			<style jsx>{`
+				.root {
+					padding: 20px 0;
+				}
+				.list-header {
+					font-weight: 800;
+					font-size: 16px;
+				}
+				.repos-title {
+					border-bottom: 1px solid #eee;
+					font-size: 24px;
+					line-height: 50px;
+				}
+				.pagination {
+					padding: 20px;
+					text-align:center;
+				}
+			`}</style>
 		</div>
 	)
 }
@@ -110,9 +157,9 @@ Search.getInitialProps = async ({ctx})=> {
 
 	let queryString = `?q=${query}`
 	if (lang) queryString += `+language:${lang}`
-	if (sort) queryString += `&sort=${sort}$order=${order||'desc'}`
+	if (sort) queryString += `&sort=${sort}&order=${order||'desc'}`
 	if (page) queryString += `&page=${page}`
-
+	queryString += `&per_page=${per_page}`
 
 	const result = await api.request({
 		url:`/search/repositories${queryString}`,
